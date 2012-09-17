ac3efc5091701cb237e3cb5e9b8a7fb79b13cc97
Sérgio Schnorr Júnior <jr.schnorr@gmail.com>
2012-09-17 14:59:21 -0300

    Flash messages

--- files/app/assets/javascripts/app.js
+++ files/app/assets/javascripts/app.js
@@ -6,5 +6,18 @@ var App = window.App = {
 
     finish: function(){
     },
+    flash: function(){
+      setTimeout( function(){
+        $('.flash').slideDown('slow')
+      }, 100)
+      if( $('.flash a').length === 0){
+        setTimeout( function(){
+          $('.flash').slideUp('slow')
+        }, 16000)
+        $(window).click( function(){
+          $('.flash').slideUp()
+        })
+      }
+    },
   }
 };
