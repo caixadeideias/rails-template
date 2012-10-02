fc09abaafd6d6c38599b37f27ca762693ce403d1
Luiz HD Costa <luizhdcosta@gmail.com>
2012-10-02 10:04:33 -0300

    Removes method_missing in controller
    method_missing is deprecated in controllers,
    we should use action_missing instead

--- files/app/controllers/users/omniauth_callbacks_controller.rb
+++ files/app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,8 +1,8 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
-  def method_missing(provider)
+  def action_missing(provider)
     omniauth = env["omniauth.auth"]
     if current_user
-      current_user.authorizations.find_or_create_by_provider_and_uid(omniauth['provider'], omniauth['uid']) 
+      current_user.authorizations.find_or_create_by_provider_and_uid(omniauth['provider'], omniauth['uid'])
       flash[:notice] = I18n.t "devise.omniauth_callbacks.success", :kind => omniauth['provider']
       redirect_to :back
     else
@@ -10,7 +10,7 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
       if authorization
         flash[:notice] = I18n.t "devise.omniauth_callbacks.success", :kind => omniauth['provider']
         sign_in_and_redirect(authorization.user, :event => :authentication)
-      else  
+      else
         session[:omniauth] = omniauth.except('extra')
         redirect_to new_user_registration_url
       end
