89171ff4a43a38f8f839a6433f1417e36f2c167c
Irio Irineu Musskopf Junior <irio.musskopf@caixadeideias.com.br>
2013-01-28 11:58:56 -0200

    Revert "Add support for heroku-accounts"
    This reverts commit 0e812d041692c370e5605b29e94943182266d56b.

--- partials/_heroku.rb
+++ partials/_heroku.rb
@@ -10,25 +10,17 @@ if would_you_like? "Configure/Create Heroku app? [y,n]".red
   config['deploy']  = would_you_like? "Deploy immediately? [y,n]".red
   config['domain']  = ask "Add custom domain(customdomain.com) or leave blank".red
 
-  accounts_unsupported = run "heroku accounts | grep 'is not a heroku command'"
-
-  account_cmd = ""
-  unless accounts_unsupported
-    heroku_account = ask "Which Heroku account?".red
-    account_cmd = "--account #{heroku_account}"
-  end
-
-  run "heroku login" if accounts_unsupported
+  run "heroku login"
 
   say "Creating Heroku app '#{heroku_name}.heroku.com'".magenta
-  while !system("heroku create #{heroku_name} #{account_cmd}")
+  while !system("heroku create #{heroku_name}")
     heroku_name = ask "What do you want to call your app? ".red
   end
 
   if config['staging']
     staging_name = "#{heroku_name}-staging"
     say "Creating staging Heroku app '#{staging_name}.herokuapp.com'".magenta
-    while !system("heroku create #{staging_name} #{account_cmd}")
+    while !system("heroku create #{staging_name}")
       staging_name = ask "What do you want to call your staging app?".red
     end
     git :remote => "add heroku git@heroku.com:#{heroku_name}.git"
@@ -37,26 +29,26 @@ if would_you_like? "Configure/Create Heroku app? [y,n]".red
 
 
   unless config['domain'].blank?
-    run "heroku domains:add #{config['domain']} #{account_cmd}"
+    run "heroku domains:add #{config['domain']}"
   end
 
   #colaborators = ask "Add collaborators? Type the email's separated by comma.".red
 
   #colaborators.split(",").map(&:strip).each do |email|
-    #run "heroku sharing:add #{email} #{account_cmd}"
+    #run "heroku sharing:add #{email}"
   #end
 
   say "Adding heroku addon [PG Backups]...".magenta
-  run "heroku addons:add pgbackups:plus #{account_cmd}"
+  run "heroku addons:add pgbackups:plus"
 
   say "Adding heroku addon [Loggly]...".magenta
-  run "heroku addons:add loggly:mole #{account_cmd}"
+  run "heroku addons:add loggly:mole"
 
   sendgrid = ask "Add sendgrid:starter addon?".red
 
   if sendgrid
     say "Adding heroku addon [Sendgrid]...".magenta
-    run "heroku addons:add sendgrid:starter #{account_cmd}"
+    run "heroku addons:add sendgrid:starter"
   end
 
   if config['deploy']
