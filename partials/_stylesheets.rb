2a0187d646769fab388f0ce1fc78891720dd25ad
Sérgio Schnorr Júnior <jr.schnorr@gmail.com>
2013-05-28 13:37:35 -0300

    Remove some Gems and add metasearch for ActiveAdmin

--- partials/_stylesheets.rb
+++ partials/_stylesheets.rb
@@ -7,7 +7,7 @@ git :rm => 'app/assets/stylesheets/application.css'
 git :commit => "-aqm 'Renamed application.css to application.sass'"
 
 if would_you_like? "Use twitter bootstrap? [y,n]".red
-  gsub_file 'Gemfile', /#gem 'twitter-bootstrap-rails/, "gem 'twitter-bootstrap-rails"
+  gsub_file 'Gemfile', /#gem 'bootstrap-sass/, "gem 'bootstrap-sass"
 
   run "echo '\n@import bootstrap' >> app/assets/stylesheets/application.sass"
 
